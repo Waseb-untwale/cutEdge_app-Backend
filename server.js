4e9be7d82b671cfb1ff79470a24f4a6182824e52
Waseb Untwale <93330099+Waseb-untwale@users.noreply.github.com>
2024-11-28 18:23:17 +0530

    delete-img-commit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 const fs = require("fs");
+require('dotenv').config();
 const blogCtrl= require('./controllers/blogCtrl')
 const app = express();
 const port = 5000;
